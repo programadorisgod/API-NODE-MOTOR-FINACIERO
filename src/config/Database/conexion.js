8f60bacb3ad6e4e859ca24b486a7021801b4fbf0
CamiloDev <catapiascastro@gmail.com>
2023-10-19 11:58:58 -0500

    -mfix(timeMSx2)

--- src/config/Database/conexion.js
+++ src/config/Database/conexion.js
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 config()
 export const connectDB = async () => {
   try {
+    console.log('[Database] Connecting...'.magenta.bold)
     await connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
